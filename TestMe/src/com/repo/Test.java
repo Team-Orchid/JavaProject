46868e6a9296c5e2e8ad313c06de45b8abacf25f
CM <Charu@Lenovo-PC>
2022-03-01 22:37:26 +0530

    #First Commit

---
+++ TestMe/src/com/repo/Test.java
@@ -0,0 +1,12 @@
+package com.repo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This the main file of the project");
+
+	}
+
+}
